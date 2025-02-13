4b0131e2f7ea76e21eb710d6f8a5e4bcc3ff6b2c
Radi3nt <pro.radi3nt@gmail.com>
2025-02-13 18:51:56 +0100

    Hotfix for integration_error tests

--- tests.m
+++ tests.m
@@ -91,11 +91,11 @@ function tests()
         
         fprintf("\n\033[34m===== Integration Error Tests =====\033[0m\n");
         ns = [10, 20, 50, 100];
-        [C, alpha] = itg.integration_error(@(x) x.^2, 0, 1, 1/3, ns, []);
-        print_result("Integration error model", C > 0 && alpha > 0);
+        s = itg.integration_error(@(x) x.^2, 0, 1, 1/3, ns, []);
+        print_result("Integration error model", s(1) > 0 && s(2) > 0);
         
-        [C, alpha] = itg.integration_error(@(x) exp(-x), 0, 1, 1 - exp(-1), ns, []);
-        print_result("Integration error model for exp(-x)", C > 0 && alpha > 0);
+        s = itg.integration_error(@(x) exp(-x), 0, 1, 1 - exp(-1), ns, []);
+        print_result("Integration error model for exp(-x)", s(1) > 0 && s(2) > 0);
         
         fprintf("\n\033[34m===== All Tests Completed =====\033[0m\n");
     catch ME
